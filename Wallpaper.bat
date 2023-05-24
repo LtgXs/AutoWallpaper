0da13ecf469de5ece0242ca40dca02ae1338ac81
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-05-24 11:17:07 +0800

    Create Wallpaper.bat

---
+++ Wallpaper.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:Dwn
+certutil -urlcache -split -f https://api.dujin.org/bing/1920.php “%appdata\AutoWallpaper\Wallpaper.jpg”
+
+
+:Change
+reg add "hkcu\control panel\desktop" /v wallpaper /d "%appdata%\AutoWallpaper\Wallpaper.jpg" /f
+RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
