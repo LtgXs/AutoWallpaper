8bba12408946235814fd486b820d3980b5f555e4
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-05-27 08:24:31 +0800

    Change wallpaper

---
+++ Change.cpp
@@ -0,0 +1,22 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //隐藏控制台
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd, SW_HIDE);
+
+    //指定图片相对路径
+    string relative_path = "wallpaper.jpg";
+
+    //获取图片绝对路径
+    char absolute_path[MAX_PATH];
+    GetFullPathNameA(relative_path.c_str(), MAX_PATH, absolute_path, NULL);
+
+    //更换壁纸
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID)absolute_path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+
+    return 0;
+}
