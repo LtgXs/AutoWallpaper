37e27714a10ec7b1593b5483ee451af8f4950ad0
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-05-27 13:04:48 +0800

    support  user config
    *Need config.ini with right format

--- Wallpaper.bat
+++ Wallpaper.bat
@@ -1,4 +1,6 @@
 @echo off
+if exist config.ini goto config
+
 set i=0
 set name=%date:~0,4%.%date:~5,2%.%date:~8,2%
 if not exist "%appdata%\AutoWallpaper" md "%appdata%\AutoWallpaper"
@@ -89,7 +91,7 @@ echo *********************Log End*********************>>"%appdata%\AutoWallpaper
 exit
 
 :Change
-copy Change.exe "%appdata%\AutoWallpaper"
+copy /Y "%appdata%\Change.exe" "%appdata%\AutoWallpaper"
 echo [%name%-%time:~0,2%:%time:~3,2%] Download Completed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
 del "%userprofile%\desktop\wallpaper.jpg"
 del "%appdata%\AutoWallpaper\wallpaper.jpg"
@@ -106,3 +108,95 @@ exit
 :Ext
 echo [%name%-%time:~0,2%:%time:~3,2%] Today's wallpaper already existed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
 echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+
+
+
+
+
+
+
+
+
+
+:config
+set i=0
+set name=%date:~0,4%.%date:~5,2%.%date:~8,2%
+::Read Config
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "idx" ^< "%~dp0config.ini"' 
+) do set "idx=%%a"
+
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "mkt" ^< "%~dp0config.ini"' 
+) do set "mkt=%%a"
+
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "chk" ^< "%~dp0config.ini"' 
+) do set "chk=%%a"
+::Read End
+if "%idx%"=="" set idx=0
+if "%mkt%"=="" set idx=zh-CN
+if "%chk%"=="" set chk=true
+if not exist "%appdata%\AutoWallpaper" md "%appdata%\AutoWallpaper"
+if not exist "%appdata%\AutoWallpaper\%name%" md "%appdata%\AutoWallpaper\%name%"
+echo.>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo ********************Log Start********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Found config.ini switch to config mode>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Config value: %idx% %mkt% %chk% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo %chk%|find "false" >NUL && goto CDW
+echo %chk%|find "False" >NUL && goto CDW
+echo %chk%|find "FALSE" >NUL && goto CDW
+if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Exit
+
+:CDW
+ver | find "10.0." > NUL &&  goto CDwnHMsg
+goto CDwnLMsg
+
+:CDwnHMsg
+echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win8.0 or higher use powershell to download >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+:CDwnH
+echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&%idx%','%appdata%\AutoWallpaper\%name%\api.xml')
+powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&format=js^&%idx%','%appdata%\AutoWallpaper\%name%\api.json')
+if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+set url=https://www.bing.com
+set size=_UHD.jpg
+set "link=" 
+for /f "tokens=17 delims=<>" %%a in (
+    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
+) do set "link=%%a" 
+echo %link%
+set FullLink=%url%%link%%size%
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+powershell (new-object System.Net.WebClient).DownloadFile('%FullLink%','%appdata%\AutoWallpaper\%name%\%name%.jpg')
+if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+set /a i=%i%+1
+if %i% GEQ 10 goto Failed
+timeout /t 3
+echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+goto CDwnH
+
+:CDwnLMsg
+echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win7.0 or lower use certutil to download (May stopped by antiMalware software)>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+:CDwnL
+echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+certutil -urlcache -split -f https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&%idx% "%appdata%\AutoWallpaper\%name%\api.xml"
+if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+set url=https://www.bing.com
+set size=_UHD.jpg
+set "link=" 
+for /f "tokens=17 delims=<>" %%a in (
+    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
+) do set "link=%%a" 
+echo %link%
+set FullLink=%url%%link%%size%
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+certutil -urlcache -split -f %FullLink% "%appdata%\AutoWallpaper\%name%\%name%.jpg"
+if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+set /a i=%i%+1
+if %i% GEQ 10 goto Failed
+timeout /t 3
+echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+goto CDwnL
