9f3a63d3b701930bb3fefdea863cec910adcabd4
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-06-22 01:01:10 +0800

    Code optimization

--- Wallpaper.bat
+++ Wallpaper.bat
@@ -1,269 +1,136 @@
 @echo off
-if exist "%~dp0config.ini" goto config
-set i=0
-set a=0
+
+:prep
+::Read Config
+    ::Specify to return the picture from the last n days, where n is a non-negative integer (0 for today, and so on)
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "idx" ^< "%~dp0config.ini"' 
+) do set "idx=%%a"
+    ::Bing daily picture time zone
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "mkt" ^< "%~dp0config.ini"' 
+) do set "mkt=%%a"
+    ::Check if today's picture already exist or not
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "chk" ^< "%~dp0config.ini"' 
+) do set "chk=%%a"
+    ::Copy to desktop or not
+for /f "tokens=1 delims=<>" %%a in (
+    'find /i "ctd" ^< "%~dp0config.ini"' 
+) do set "ctd=%%a"
+
+::check config
+if "%idx%"=="" set idx=idx=0
+if "%mkt%"=="" set mkt=mkt=zh-CN
+if "%chk%"=="" set chk=chk=true
+if "%ctd%"=="" set ctd=ctd=true
+
+::check system ver
+ver | find "5.1." > NUL && set IsWin10=0
+ver | find "6.1." > NUL && set IsWin10=0
+ver | find "5.2." > NUL && set IsWin10=0
+ver | find "6.2." > NUL && set IsWin10=1
+ver | find "6.3." > NUL && set IsWin10=1
+ver | find "10.0." >NUL && set IsWin10=1
+::set
 set name=%date:~0,4%.%date:~5,2%.%date:~8,2%
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Ext
-if not exist "%appdata%\AutoWallpaper" md "%appdata%\AutoWallpaper"
-if not exist "%appdata%\AutoWallpaper\%name%" md "%appdata%\AutoWallpaper\%name%"
+set BingApi=https://www.bing.com/HPImageArchive.aspx?n=1
+set w10=echo %IsWin10%^|find "1" ^>NUL ^&^&
+set w7=echo %IsWin10%^|find "0" ^>NUL ^&^&
+set folder="%appdata%\AutoWallpaper"
+set dfolder=%appdata%\AutoWallpaper\%name%
+set i=0
 
-echo.>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-echo ********************Log Start********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-ver | find "10.0." > NUL &&  goto DwnHMsg
-goto DwnLMsg
 
-:DwnHMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win8.0 or higher use powershell to download >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:DwnH
-echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&mkt=zh-CN','%appdata%\AutoWallpaper\%name%\api.xml')
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&mkt=zh-CN^&format=js^&idx=0','%appdata%\AutoWallpaper\%name%\api.json')
-if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+
+:Main
+::create work folder
+if not exist %folder% md %folder%
+::create daily picture folder
+if not exist %dfolder% md %dfolder%
+echo.>>"%dfolder%\%name%.log"
+echo ********************Log Start********************>>"%dfolder%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Config value: %idx% %mkt% %chk% %ctd% >>"%dfolder%\%name%.log"
+::Options "Check if the picture has been already or not"
+echo %chk%|find "false" >NUL && goto DwnMsg
+if exist "%dfolder%\%name%.jpg" goto Ext
+
+:DwnMsg
+%w10% echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win8.0 or higher use powershell to download >>"%dfolder%\%name%.log"
+%w7% echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win7.0 or lower use certutil to download (May stopped by antiMalware software)>>"%dfolder%\%name%.log"
+
+:DwnApi
+echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%dfolder%\%name%.log"
+::win10 powershell
+%w10% powershell (new-object System.Net.WebClient).DownloadFile('%BingApi%^&%mkt%^&%idx%','%dfolder%\api.xml')
+%w10% powershell (new-object System.Net.WebClient).DownloadFile('%BingApi%^&%mkt%^&format=js^&%idx%','%dfolder%\api.json')
+::win7 certutil
+%w7% certutil -urlcache -split -f %BingApi%^&%mkt%^&%idx% "%dfolder%\api.xml"
+%w7% certutil -urlcache -split -f %BingApi%^&%mkt%^&format=js^&%idx% "%dfolder%\api.json"
+::check download successful or not
+if exist "%dfolder%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%dfolder%\%name%.log"
+if exist "%dfolder%\api.xml" goto Dwn
+::if not successful try again for 10 times
+if not exist "%dfolder%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Failed to get download link from bing api >>"%dfolder%\%name%.log"
+set /a a=%a%+1
+if %a% GEQ 10 goto Failed
+timeout /t 3
+echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%a%/10) >>"%dfolder%\%name%.log"
+goto DwnAPI
+
+:Dwn
+::Gather download link
 set url=https://www.bing.com
 set size=_UHD.jpg
 set "link=" 
+    ::Read link from api.xml
 for /f "tokens=17 delims=<>" %%a in (
-    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
-) do set "link=%%a" 
+    'find /i "<images>" ^< "%dfolder%\api.xml"' 
+) do set "link=%%a"
 echo %link%
+    ::Output the full link
 set FullLink=%url%%link%%size%
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-powershell (new-object System.Net.WebClient).DownloadFile('%FullLink%','%appdata%\AutoWallpaper\%name%\%name%.jpg')
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a i=%i%+1
-if %i% GEQ 10 goto BackupHMsg
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto DwnH
-
-:DwnLMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win7.0 or lower use certutil to download (May stopped by antiMalware software)>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:DwnL
-echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-certutil -urlcache -split -f https://www.bing.com/HPImageArchive.aspx?n=1^&mkt=zh-CN "%appdata%\AutoWallpaper\%name%\api.xml"
-if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set url=https://www.bing.com
-set size=_UHD.jpg
-set "link=" 
-for /f "tokens=17 delims=<>" %%a in (
-    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
-) do set "link=%%a" 
-echo %link%
-set FullLink=%url%%link%%size%
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-certutil -urlcache -split -f %FullLink% "%appdata%\AutoWallpaper\%name%\%name%.jpg"
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a i=%i%+1
-if %i% GEQ 10 goto BackupHMsg
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto DwnL
-
-:BackupHMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download From Another API >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:BackupH
-set i=0
-powershell (new-object System.Net.WebClient).DownloadFile('https://api.dujin.org/bing/1920.php','%appdata%\AutoWallpaper\%name%\%name%.jpg')
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%dfolder%\%name%.log"
+::Download picture from gathered link
+%w10% powershell (new-object System.Net.WebClient).DownloadFile('%FullLink%','%dfolder%\%name%.jpg')
+%w7% certutil -urlcache -split -f %FullLink% "%dfolder%\%name%.jpg"
+::check download successful or not
+if exist "%dfolder%\%name%.jpg" goto Change
+::if not successful try again for 10 times
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%dfolder%\%name%.log"
 set /a i=%i%+1
 if %i% GEQ 10 goto Failed
 timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto BackupH
-
-:BackupLMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download From Another API >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:BackupL
-set i=0
-certutil -urlcache -split -f https://api.dujin.org/bing/1920.php "%appdata%\AutoWallpaper\%name%\%name%.jpg"
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a i=%i%+1
-if %i% GEQ 10 goto Failed
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto BackupL
-
-:Failed
-echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-exit
+echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%dfolder%\%name%.log"
+goto Dwn
 
 :Change
 ::Use Change.vbs
-ver | find "5.2." > NUL && copy /Y "%appdata%\Change.vbs" "%appdata%\AutoWallpaper"
-ver | find "6.1." > NUL && copy /Y "%appdata%\Change.vbs" "%appdata%\AutoWallpaper"
-ver | find "5.2." > NUL && copy /Y "%appdata%\Change.vbs" "%appdata%\AutoWallpaper"
+%w7% copy /Y "%appdata%\Change.vbs" %folder%
 ::Use Change.exe
-ver | find "6.2." > NUL && copy /Y "%appdata%\Change.exe" "%appdata%\AutoWallpaper"
-ver | find "6.3." > NUL && copy /Y "%appdata%\Change.exe" "%appdata%\AutoWallpaper"
-ver | find "10.0." > NUL && copy /Y "%appdata%\Change.exe" "%appdata%\AutoWallpaper"
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Completed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-
+%w10% copy /Y "%appdata%\Change.exe" %folder%
+echo [%name%-%time:~0,2%:%time:~3,2%] Download Completed >>"%dfolder%\%name%.log"
 ::Options "Copy to desktop or not"
-echo %ctd%|find "false" >NUL && goto ChangeFC
-echo %ctd%|find "False" >NUL && goto ChangeFC
-echo %ctd%|find "FALSE" >NUL && goto ChangeFC
-
+echo %ctd%|find "true" >NUL && set IsCtd=1
 del "%userprofile%\desktop\wallpaper.jpg"
 del "%appdata%\AutoWallpaper\wallpaper.jpg"
-copy "%appdata%\AutoWallpaper\%name%\%name%.jpg" "%userprofile%\desktop"
-ren "%userprofile%\desktop\%name%.jpg" "wallpaper.jpg"
-copy "%userprofile%\desktop\wallpaper.jpg" "%appdata%\AutoWallpaper"
-cd "%appdata%\AutoWallpaper"
-ver | find "5.1." > NUL && Change.vbs
-ver | find "6.1." > NUL && Change.vbs
-ver | find "5.2." > NUL && Change.vbs
-ver | find "6.2." > NUL && Change.exe
-ver | find "6.3." > NUL && Change.exe
-ver | find "10.0." >NUL && Change.exe
-RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
-echo [%name%-%time:~0,2%:%time:~3,2%] Change Wallpaper Completed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-exit
-
-:ChangeFC
-del "%appdata%\AutoWallpaper\wallpaper.jpg"
-copy "%appdata%\AutoWallpaper\%name%\%name%.jpg" "%appdata%\AutoWallpaper"
+copy "%dfolder%\%name%.jpg" "%appdata%\AutoWallpaper"
 ren "%appdata%\AutoWallpaper\%name%.jpg" "wallpaper.jpg"
-cd "%appdata%\AutoWallpaper"
-ver | find "5.1." > NUL && Change.vbs
-ver | find "6.1." > NUL && Change.vbs
-ver | find "5.2." > NUL && Change.vbs
-ver | find "6.2." > NUL && Change.exe
-ver | find "6.3." > NUL && Change.exe
-ver | find "10.0." >NUL && Change.exe
+echo %IsCtd%|find "1" && copy "%appdata%\AutoWallpaper\wallpaper.jpg" "%userprofile%\desktop"
+
+cd %folder%
+%w7% Change.vbs
+%w10% Change.exe
 RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
-echo [%name%-%time:~0,2%:%time:~3,2%] Change Wallpaper Completed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Change Wallpaper Completed >>"%dfolder%\%name%.log"
 echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
 exit
 
 :Ext
-echo [%name%-%time:~0,2%:%time:~3,2%] Today's wallpaper already existed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Today's wallpaper already existed >>"%dfolder%\%name%.log"
 echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
 exit
 
-
-
-
-
-
-
-
-
-
-:config
-set i=0
-set name=%date:~0,4%.%date:~5,2%.%date:~8,2%
-::Read Config
-for /f "tokens=1 delims=<>" %%a in (
-    'find /i "idx" ^< "%~dp0config.ini"' 
-) do set "idx=%%a"
-
-for /f "tokens=1 delims=<>" %%a in (
-    'find /i "mkt" ^< "%~dp0config.ini"' 
-) do set "mkt=%%a"
-
-for /f "tokens=1 delims=<>" %%a in (
-    'find /i "chk" ^< "%~dp0config.ini"' 
-) do set "chk=%%a"
-
-for /f "tokens=1 delims=<>" %%a in (
-    'find /i "ctd" ^< "%~dp0config.ini"' 
-) do set "ctd=%%a"
-::Read End
-
-::check config
-if "%idx%"=="" set idx=0
-if "%mkt%"=="" set idx=zh-CN
-if "%chk%"=="" set chk=true
-if "%ctd%"=="" set ctd=true
-::create work folder
-if not exist "%appdata%\AutoWallpaper" md "%appdata%\AutoWallpaper"
-::create daily picture folder
-if not exist "%appdata%\AutoWallpaper\%name%" md "%appdata%\AutoWallpaper\%name%"
-echo.>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-echo ********************Log Start********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-echo [%name%-%time:~0,2%:%time:~3,2%] Found config.ini switch to config mode>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-echo [%name%-%time:~0,2%:%time:~3,2%] Config value: %idx% %mkt% %chk% %ctd% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-::Options "Check if the picture has been already or not"
-echo %chk%|find "false" >NUL && goto CDW
-echo %chk%|find "False" >NUL && goto CDW
-echo %chk%|find "FALSE" >NUL && goto CDW
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Ext
-
-:CDW
-ver | find "10.0." > NUL &&  goto CDwnHMsg
-goto CDwnLMsg
-
-:CDwnHMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win8.0 or higher use powershell to download >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:CDwnHApi
-echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&%idx%','%appdata%\AutoWallpaper\%name%\api.xml')
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&format=js^&%idx%','%appdata%\AutoWallpaper\%name%\api.json')
-if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-if exist "%appdata%\AutoWallpaper\%name%\api.xml" goto CDwnH
-if not exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Failed to get download link from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a a=%a%+1
-if %a% GEQ 10 goto Failed
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%a%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto CDwnHAPI
-:CDwnH
-::Gather download link
-set url=https://www.bing.com
-set size=_UHD.jpg
-set "link=" 
-::Read link from api.xml
-for /f "tokens=17 delims=<>" %%a in (
-    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
-) do set "link=%%a"
-echo %link%
-::Output the full link
-set FullLink=%url%%link%%size%
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-powershell (new-object System.Net.WebClient).DownloadFile('%FullLink%','%appdata%\AutoWallpaper\%name%\%name%.jpg')
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a i=%i%+1
-if %i% GEQ 10 goto Failed
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto CDwnH
-
-:CDwnLMsg
-echo [%name%-%time:~0,2%:%time:~3,2%] System version is Win7.0 or lower use certutil to download (May stopped by antiMalware software)>>"%appdata%\AutoWallpaper\%name%\%name%.log"
-:CDwnLAPI
-echo [%name%-%time:~0,2%:%time:~3,2%] Getting url from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-certutil -urlcache -split -f https://www.bing.com/HPImageArchive.aspx?n=1^&%mkt%^&%idx% "%appdata%\AutoWallpaper\%name%\api.xml"
-if exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Successful >>"%appdata%\AutoWallpaper\%name%\%name%.log" && goto CDwnL
-if not exist "%appdata%\AutoWallpaper\%name%\api.xml" echo [%name%-%time:~0,2%:%time:~3,2%] Failed to get download link from bing api >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a a=%a%+1
-if %a% GEQ 10 goto Failed
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%a%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto CDwnLAPI
-:CDwnL
-::Gather download link
-set url=https://www.bing.com
-set size=_UHD.jpg
-set "link=" 
-::Read link from api.xml
-for /f "tokens=17 delims=<>" %%a in (
-    'find /i "<images>" ^< "%appdata%\AutoWallpaper\%name%\api.xml"' 
-) do set "link=%%a"
-echo %link%
-::Output the full link
-set FullLink=%url%%link%%size%
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-certutil -urlcache -split -f %FullLink% "%appdata%\AutoWallpaper\%name%\%name%.jpg"
-if exist "%appdata%\AutoWallpaper\%name%\%name%.jpg" goto Change
-echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-set /a i=%i%+1
-if %i% GEQ 10 goto Failed
-timeout /t 3
-echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%appdata%\AutoWallpaper\%name%\%name%.log"
-goto CDwnL
+:Failed
+echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
+exit
