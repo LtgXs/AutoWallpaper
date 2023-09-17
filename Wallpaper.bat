b2fe5ebe6450e6b167eddcb8b3fc11ef12a8e845
Little Gao <98456232+LittleGaofx@users.noreply.github.com>
2023-09-17 19:09:19 +0800

    Update to 1.6.0
    Update to 1.6.0

--- Wallpaper.bat
+++ Wallpaper.bat
@@ -1,5 +1,4 @@
 @echo off
-
 :prep
 ::Read Config
     ::Specify to return the picture from the last n days, where n is a non-negative integer (0 for today, and so on)
@@ -22,30 +21,18 @@ for /f "tokens=1 delims=<>" %%a in (
 for /f "tokens=1 delims=<>" %%a in (
     'find /i "wtm" ^< "%~dp0config.ini"' 
 ) do set "wtm=%%a"
-    ::Watermark path(suggest to use .png)
+    ::Watermark Count
 for /f "tokens=2 delims=<>" %%a in (
-    'find /i "wmp" ^< "%~dp0config.ini"' 
-) do set "wmp=%%a"
-    ::Watermark POS
-for /f "tokens=2 delims=<>" %%a in (
-    'find /i "wPosX" ^< "%~dp0config.ini"' 
-) do set "wPosX=%%a"
-for /f "tokens=2 delims=<>" %%a in (
-    'find /i "wPosY" ^< "%~dp0config.ini"' 
-) do set "wPosY=%%a"
+    'find /i "wtc" ^< "%~dp0config.ini"' 
+) do set "wtc=%%a"
 
 ::check config
 if "%idx%"=="" set idx=idx=0
 if "%mkt%"=="" set mkt=mkt=zh-CN
 if "%chk%"=="" set chk=chk=true
 if "%ctd%"=="" set ctd=ctd=true
-if "%wtm%"=="" set wtm=wtm=true
-if "%wmp%"=="" set wmp=%appdata%\watermark.png
-if "%wPosY%"=="" set wPosY=wPosY=1.2
-if "%wPosX%"=="" set wPosX=wPosX=2
-set wmp=%wmp:~4%
-set wPosX=%wPosX:~6%
-set wPosY=%wPosY:~6%
+if "%wtm%"=="" set wtm=wtm=false
+if "%wtc%"=="" set wtc=wtc=1
 if not "%wtm%"=="wtm=true" set wtm=wtm=false
 
 ::check system ver
@@ -62,6 +49,7 @@ set w10=echo %IsWin10%^|find "1" ^>NUL ^&^&
 set w7=echo %IsWin10%^|find "0" ^>NUL ^&^&
 set folder="%appdata%\AutoWallpaper"
 set dfolder=%appdata%\AutoWallpaper\%name%
+set watermarkF=%appdata%\Autowallpaper\watermark
 set i=0
 
 
@@ -73,7 +61,7 @@ if not exist %folder% md %folder%
 if not exist %dfolder% md %dfolder%
 echo.>>"%dfolder%\%name%.log"
 echo ********************Log Start********************>>"%dfolder%\%name%.log"
-echo [%name%-%time:~0,2%:%time:~3,2%] Config value: %idx% %mkt% %chk% %ctd% %wtm% wmp=%wmp%>>"%dfolder%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Config value: %idx% %mkt% %chk% %ctd% %wtm% %wtc% >>"%dfolder%\%name%.log"
 ::Options "Check if the picture has been already or not"
 echo %chk%|find "false" >NUL && goto DwnMsg
 if exist "%dfolder%\%name%.jpg" goto Ext
@@ -118,7 +106,7 @@ echo [%name%-%time:~0,2%:%time:~3,2%] Download Link: %FullLink% >>"%dfolder%\%na
 %w10% powershell (new-object System.Net.WebClient).DownloadFile('%FullLink%','%dfolder%\%name%.jpg')
 %w7% certutil -urlcache -split -f %FullLink% "%dfolder%\%name%.jpg"
 ::check download successful or not
-if exist "%dfolder%\%name%.jpg" goto Change
+if exist "%dfolder%\%name%.jpg" goto Watermark
 ::if not successful try again for 10 times
 echo [%name%-%time:~0,2%:%time:~3,2%] Download Failed >>"%dfolder%\%name%.log"
 set /a i=%i%+1
@@ -126,28 +114,64 @@ if %i% GEQ 10 goto Failed
 timeout /t 3
 echo [%name%-%time:~0,2%:%time:~3,2%] Try Download Again (%i%/10) >>"%dfolder%\%name%.log"
 goto Dwn
-
-:Change
-::Use Change.vbs
-%w7% copy /Y "%appdata%\Change.vbs" %folder%
-::Use Change.exe
-%w10% copy /Y "%appdata%\Change.exe" %folder%
 echo [%name%-%time:~0,2%:%time:~3,2%] Download Completed >>"%dfolder%\%name%.log"
 del "%userprofile%\desktop\wallpaper.jpg"
 del "%appdata%\AutoWallpaper\wallpaper.jpg"
 
-::add watermark
-echo %wtm%|find "true" && cd "%appdata%\AutoWallpaper\main" && ffmpeg -i "%dfolder%\%name%.jpg" -i "%wmp%" -filter_complex "overlay=(main_w-overlay_w)/%wPosX%:(main_h-overlay_h)/%wPosY%" -q:v 0 "%appdata%\AutoWallpaper\wallpaper.jpg" & if not exist "%appdata%\AutoWallpaper\wallpaper.jpg" ffmpeg -i "%dfolder%\%name%.jpg" -i "%appdata%\watermark.png" -filter_complex "overlay=(main_w-overlay_w)/%wPosX%:(main_h-overlay_h)/%wPosY%" -q:v 0 "%appdata%\AutoWallpaper\wallpaper.jpg"
-::none watermark
-echo %wtm%|find "false" && copy "%dfolder%\%name%.jpg" "%appdata%\AutoWallpaper"
-echo %wtm%|find "false" && ren "%appdata%\AutoWallpaper\%name%.jpg" "wallpaper.jpg"
-::Copy to desktop or not
-echo %ctd%|find "true" && copy "%appdata%\AutoWallpaper\wallpaper.jpg" "%userprofile%\desktop"
+:Watermark
+set wtc=%wtc:~4%
+set ii=0
+::add copyright watermark
+cd "%appdata%\AutoWallpaper\main" && ffmpeg -i "%dfolder%\%name%.jpg" -i "%appdata%\watermark.png" -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/1.2" -q:v 0 "%appdata%\AutoWallpaper\wallpaper.jpg"
+echo %wtm%|find "true" && goto WatermarkLoop
+goto Change
 
+:WatermarkLoop
+set /a ii=%ii%+1
+echo %ii%
+if %ii%=1 echo.>>"%dfolder%\%name%.log"
+echo [%name%-%time:~0,2%:%time:~3,2%] Loading custom watermark %ii% >>"%dfolder%\%name%.log"
+::Read Config
+    ::Watermark path(suggest to use .png)
+for /f "tokens=2 delims=<>" %%a in (
+    'find /i "wmp%ii%" ^< "%~dp0config.ini"' 
+) do set "wmp=%%a"
+    ::Watermark POS
+for /f "tokens=2 delims=<>" %%a in (
+    'find /i "wPosX%ii%" ^< "%~dp0config.ini"' 
+) do set "wPosX=%%a"
+for /f "tokens=2 delims=<>" %%a in (
+    'find /i "wPosY%ii%" ^< "%~dp0config.ini"' 
+) do set "wPosY=%%a"
+if "%wmp%"=="" set wmp%i%=%appdata%\watermark.png
+if "%wPosY%"=="" set wPosY%i%=wPosY=1.2
+if "%wPosX%"=="" set wPosX%i%=wPosX=2
+if "%wtc%"=="" set wtc=1
+set wmp=%wmp:~5%
+set wPosX=%wPosX:~7%
+set wPosY=%wPosY:~7%
+::add custom watermark
+if exist %wmp% echo [%name%-%time:~0,2%:%time:~3,2%] Custom watermark %ii% Loaded >>"%dfolder%\%name%.log"
+if exist %wmp% echo [%name%-%time:~0,2%:%time:~3,2%] Path: %wmp%     XPos: %wPosX%     YPos: %wPosY% >>"%dfolder%\%name%.log"
+if not exist %wmp% echo [%name%-%time:~0,2%:%time:~3,2%] Failed to load custom watermark Fallback to default value>>"%dfolder%\%name%.log"
+if not exist %wmp% set wmp=%appdata%\watermark.png
+ffmpeg -i "%appdata%\AutoWallpaper\wallpaper.jpg" -i "%wmp%" -filter_complex "overlay=(main_w-overlay_w)/%wPosX%:(main_h-overlay_h)/%wPosY%" -q:v 0 "%appdata%\AutoWallpaper\main\wallpaper.jpg"
+move /Y "%appdata%\AutoWallpaper\main\wallpaper.jpg" "%appdata%\AutoWallpaper\wallpaper.jpg"
+if %ii% GEQ %wtc% echo.>>"%dfolder%\%name%.log"
+if %ii% GEQ %wtc% goto Change
+goto WatermarkLoop
 
+:Change
+echo.>>"%dfolder%\%name%.log"
+::Windows 7 Use Change.vbs
+%w7% copy /Y "%appdata%\Change.vbs" %folder%
+::Windows 10 Use Change.exe
+%w10% copy /Y "%appdata%\Change.exe" %folder%
 cd %folder%
 %w7% Change.vbs
 %w10% Change.exe
+::Copy to desktop or not
+echo %ctd%|find "true" && copy "%appdata%\AutoWallpaper\wallpaper.jpg" "%userprofile%\desktop"
 RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters
 echo [%name%-%time:~0,2%:%time:~3,2%] Change Wallpaper Completed >>"%dfolder%\%name%.log"
 echo *********************Log End*********************>>"%appdata%\AutoWallpaper\%name%\%name%.log"
